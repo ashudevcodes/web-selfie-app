d5bed88e0cc19dddbb31a4ac7e678340036eacb9
ashudevcodes <ashishprasad949@gmail.com>
2024-12-03 19:26:59 +0530

    add: map
    - add map to see the real time cordinates

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 
 # OR
 - After installing node packages just run `npm run dev` it will automatically open browser and application
+
+# Feature
+- use in user Local storage Database
+- store user location in Database.db file
+- store user picture in Database
+- open application in browser automatically
+
+# How Its Look
+<kbd><img src="./assets/Screenshot From 2024-12-03 19-19-27.png"><kbd>
+<br>
+<kbd><img src="./assets/Screenshot From 2024-12-03 19-19-39.png"><kbd>
--- public/script.js
+++ public/script.js
@@ -1,4 +1,9 @@
+const tileURL = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+const attribution = '& copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+const issURL = "https://api.wheretheiss.at/v1/satellites/25544"
+
 setup()
+
 function setup() {
     if ('geolocation' in navigator) {
         console.log("geoLocatioan is available")
@@ -6,8 +11,12 @@ function setup() {
             noCanvas()
             const video = createCapture(VIDEO)
             video.size(320, 240)
+            document.querySelector('video').id = "video_i"
             const latitude = position.coords.latitude
             const longitude = position.coords.longitude
+            const map = L.map('map').setView([latitude, longitude], 13)
+            L.tileLayer(tileURL, { attribution }).addTo(map)
+            L.marker([latitude, longitude]).addTo(map);
             document.getElementById('latitude').textContent = latitude
             document.getElementById('longitude').textContent = longitude
             const btn = document.getElementById('sendloc').addEventListener('click', async () => {
