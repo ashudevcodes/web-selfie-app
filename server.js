704f74e72e6294e47ddf863006e7f5b51a68a334
ashudevcodes <ashishprasad949@gmail.com>
2024-12-04 21:31:26 +0530

    add: weather data to show on website

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const fetch = require('node-fetch')
 const { exec } = require('child_process');
 const os = require('os')
-const DataStore = require('nedb')
+const DataStore = require('nedb');
 const app = express()
 const PORT = 6969
 app.use(express.static('public'))
@@ -22,6 +23,7 @@ app.post('/api', (req, res) => {
         latitude: data.latitude,
         longitube: data.longitube,
         name: data.name,
+        temperature: data.temperature,
         image: data.image64,
     })
     res.end()
@@ -37,19 +39,37 @@ app.get('/api', (req, res) => {
     })
 })
 
+app.get('/api/weather/:latlon', async (req, res) => {
+    const latlon = req.params.latlon.split(',')
+    const latitude = latlon[0]
+    const longitude = latlon[1]
+    const weatherURL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`
+    const wetApiRes = await fetch(weatherURL)
+    const wetJson = await wetApiRes.json()
+    console.log(wetJson)
+    res.json(wetJson)
+})
+
+let openState = true
 app.listen(PORT, () => {
     console.log(`Application Listen at PORT ${PORT}`)
-    switch (os.platform()) {
-        case 'linux':
-            exec(`xdg-open http://localhost:${PORT}`)
-            break;
-        case 'win32':
-            exec(`start http://localhost:${PORT}`)
-            break;
-        case 'darwin':
-            exec(`open http://localhost:${PORT}`)
-            break;
-        default:
-            break;
+    console.log(openState)
+    if (openState) {
+        switch (os.platform()) {
+            case 'linux':
+                exec(`xdg-open http://localhost:${PORT}`)
+                openState = false
+                break;
+            case 'win32':
+                exec(`start http://localhost:${PORT}`)
+                openState = false
+                break;
+            case 'darwin':
+                exec(`open http://localhost:${PORT}`)
+                openState = false
+                break;
+            default:
+                break;
+        }
     }
 })
